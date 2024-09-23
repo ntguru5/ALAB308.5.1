f79ee05ce9235bbfe2a61d6b0c2fefbee31fed72
Ken Yeh <yehken@gmail.com>
2024-09-23 13:43:54 -0700

    return longest string

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Take an array of numbers and return the sum.
+// ========= Take an array of numbers and return the sum.
 function sumArray(numbers){
     return numbers.reduce((total, current) => total + current, 0);
 }
@@ -6,15 +6,25 @@ function sumArray(numbers){
 const numbers = [1, 2, 3, 4, 5];
 console.log(sumArray(numbers)); // Output: 15
 
-// Take an array of numbers and return the average.
+// ========= Take an array of numbers and return the average.
 function avgArray(numbers) {
     const sum = numbers.reduce((total, current) => total + current, 0);
     return sum / numbers.length;
 }
-const numbers2 = [1, 2, 3, 4, 5];
-console.log(avgArray(numbers));
 
-// Take an array of strings and return the longest string.
-// Take an array of strings, and a number and return an array of the strings that are longer than the given number.
+const numbers2 = [1, 2, 3, 4, 5];
+console.log(avgArray(numbers)); // Output: 3
+
+// ========= Take an array of strings and return the longest string.
+function longestString(strings) {
+    return strings.reduce((longest, current) =>
+        current.length > longest.length ? current : longest, ""
+    );
+}
+
+const strings = ["apple", "banana", "grape", "strawberry"];
+console.log(longestString(strings));
+
+// ========= Take an array of strings, and a number and return an array of the strings that are longer than the given number.
 // For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
-// Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+// ========= Take a number, n, and print every number between 1 and n without using loops. Use recursion.
